c28ee53fc7db564e293cdcd5e5435b85d667d449
Daniel Sung <danielsung@daniels-mbp-54.wifi.local.cmu.edu>
2023-11-11 15:30:47 -0500

    added AddFriend page

--- lib/Screens/feed/feed_page.dart
+++ lib/Screens/feed/feed_page.dart
@@ -1,4 +1,123 @@
 import 'package:flutter/material.dart';
+
+class AddFriendPage extends StatelessWidget {
+  const AddFriendPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleSpacing: 5,
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back_ios_new_outlined,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+        ),
+        body: Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Column(
+              children: [
+                Center(
+                  child: Container(
+                      height: MediaQuery.of(context).size.height * 0.05,
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          border: Border.all(color: Colors.black, width: 1.3)),
+                      child: Container(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("Add or search friends"),
+                          ))),
+                ),
+                SizedBox(height: 20),
+                Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text("ADD YOUR CONTACTS")),
+                Container(
+                    height: MediaQuery.of(context).size.height * 0.7,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    alignment: Alignment.center,
+                    child: ListView.builder(
+                        itemCount: 5,
+                        prototypeItem: const AddFriendComp(),
+                        itemBuilder: (context, index) {
+                          return AddFriendComp();
+                        })),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class AddFriendComp extends StatelessWidget {
+  const AddFriendComp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Container(
+              foregroundDecoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: AssetImage('assets/images/sashimigym.png'),
+                    fit: BoxFit.cover),
+              ),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.blueGrey, width: 0.1)),
+              width: MediaQuery.of(context).size.width * 0.1,
+              height: MediaQuery.of(context).size.width * 0.1,
+            ),
+            SizedBox(width: 7),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [Text("Steven"), Text("steve123")],
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            IconButton(
+              iconSize: 25,
+              icon: Icon(Icons.person_add_alt),
+              onPressed: () {
+                print("add button pressed");
+              },
+              splashRadius: 20,
+            ),
+            IconButton(
+              iconSize: 25,
+              icon: Icon(Icons.close),
+              onPressed: () {
+                print("close button pressed");
+              },
+              splashRadius: 20,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
 class FeedPage extends StatefulWidget {
   const FeedPage({super.key});
 
@@ -23,21 +142,31 @@ class _FeedPageState extends State<FeedPage> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
+        titleSpacing: 5,
         automaticallyImplyLeading: false,
-        leading: IconButton(
-          icon: const Icon(Icons.search_outlined, color: Colors.black, size: 50),
-          onPressed: () {
-            print("search button pressed!");
-          },
-        ),
-        actions: <Widget>[
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           IconButton(
-            icon: const Icon(Icons.person_add, color: Colors.black, size: 50),
+            icon: Icon(Icons.search_outlined, color: Colors.black, size: 35),
             onPressed: () {
-              print("Add button pressed!");
+              print("search button pressed!");
             },
+            splashRadius: 25,
           ),
-        ],
+          Spacer(),
+          IconButton(
+            icon: Icon(Icons.person_add_alt_1_sharp,
+                color: Colors.black, size: 35),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddFriendPage()),
+              );
+              print("search button pressed!");
+            },
+            splashRadius: 25,
+          ),
+        ]),
       ),
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
@@ -121,21 +250,21 @@ class FeedContent extends StatelessWidget {
                       children: [
                         Row(children: [
                           IconButton(
-                              icon: const Icon(Icons.account_box_rounded),
+                              icon: Icon(Icons.account_box_rounded),
                               onPressed: () {
                                 print("account button pressed!");
                               }),
-                          const SizedBox(width: 20),
+                          SizedBox(width: 20),
                           Text(
                             acc_Name,
-                            style: const TextStyle(
+                            style: TextStyle(
                               fontSize: 18, // Change the font size as needed
                             ),
                           ),
                         ]),
-                        const SizedBox(width: 115),
+                        SizedBox(width: 115),
                         IconButton(
-                            icon: const Icon(Icons.more_horiz, size: 30),
+                            icon: Icon(Icons.more_horiz, size: 30),
                             onPressed: () {
                               print("more info button pressed!");
                             }),
@@ -162,12 +291,12 @@ class FeedContent extends StatelessWidget {
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 IconButton(
-                                    icon: const Icon(Icons.favorite_border),
+                                    icon: Icon(Icons.favorite_border),
                                     onPressed: () {
                                       print("like button pressed!");
                                     }),
                                 IconButton(
-                                    icon: const Icon(Icons.emoji_emotions_outlined),
+                                    icon: Icon(Icons.emoji_emotions_outlined),
                                     onPressed: () {
                                       print("react button pressed!");
                                     }),
