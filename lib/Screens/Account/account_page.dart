da0cc705341f985fde7b54c11fe6abccb4e34ede
jyuwang <100096661+jyuwang@users.noreply.github.com>
2024-02-10 17:18:08 -0500

    For accountPage: Add explain comments, restructure readJson call, clean up

--- lib/Screens/Account/account_page.dart
+++ lib/Screens/Account/account_page.dart
@@ -1,52 +1,61 @@
-import 'package:flutter/material.dart';
+// Import necessary packages and files
 import 'profile.dart';
-// import 'photo.dart';
-// import 'top.dart';
-// import 'edit.dart';
 import 'midNav.dart';
-// import 'bio.dart';
-// import 'nameTag.dart';
-// import 'name.dart';
-// import 'pronouns.dart';
 import 'posts.dart';
 import 'dart:convert';
 import 'dart:async';
 import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 
-// await Firebase.initializeApp(
-//   options: DefaultFirebaseOptions.currentPlatform,
-// );
-
+// Main widget for the Account Page
 class AccountPage extends StatefulWidget {
+  // Constructor for AccountPage, takes a key as an argument
   const AccountPage({super.key});
 
+  // Create the mutable state for this widget
   @override
   State<AccountPage> createState() => _AccountPageState();
 }
 
+// The mutable state for the AccountPage widget
 class _AccountPageState extends State<AccountPage> {
+  // A list to hold the data read from the JSON file
   List _items = [];
 
-  // Fetch content from the json file
+  // Asynchronous function to read data from a JSON file
+  // The JSON file is located in the app's assets
+  // The function decodes the JSON file and updates _items with the "posts" data
   Future<void> readJson() async {
+    // Load the JSON file as a string
     final String response =
         await rootBundle.loadString('assets/data/sampleData.json');
+    // Decode the JSON string
     final data = await json.decode(response);
+    // Update _items with the "posts" data and notify the framework
     setState(() {
       _items = data["posts"];
-      // print("The number of posts are ${_items.length}");
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+    // Call readJson in initState instead of build
+    readJson();
+  }
+
+  // Define the user interface of the AccountPage
+  @override
   Widget build(BuildContext context) {
+    // Hardcoded data for the user's profile
     const username = '@gtang';
     const image = 'assets/images/GracePfp.png'; // Local asset path
     const name = 'Grace';
     const pronouns = 'she/her';
     const biography = 'CS @ CMU 2024; TJ Graduate :))))';
 
-    readJson();
+    // Load the data from the JSON file
+    // readJson();
 
     return MaterialApp(
       home: Scaffold(
