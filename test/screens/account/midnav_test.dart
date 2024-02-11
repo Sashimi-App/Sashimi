827ca715edcb2b400642e6234583ce887441cbea
Skam23 <shingk@andrew.cmu.edu>
2024-02-10 19:49:16 -0500

    Set up empty test to avoid comilation errors

--- test/screens/account/midnav_test.dart
+++ test/screens/account/midnav_test.dart
@@ -1,1 +1,8 @@
 
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Empty test to avoid compilation errors', (WidgetTester tester) async {
+    expect(true, isTrue);
+  });
+}
---
+++ test/screens/toDoList/photo_nocam_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Empty test to avoid compilation errors', (WidgetTester tester) async {
+    expect(true, isTrue);
+  });
+}
